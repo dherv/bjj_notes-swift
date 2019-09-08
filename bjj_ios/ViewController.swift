ab3181c664d218efd5b092936ca6665132814c21
dherv <damien.herv@gmail.com>
2019-09-08 14:01:29 +0900

    Initial Commit

---
+++ bjj_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bjj_ios
+//
+//  Created by Damien Hervieux on 2019/09/08.
+//  Copyright © 2019 Damien Hervieux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
